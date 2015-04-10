71014d8399d47d32427dbfdb98e95ea669e1ab17
1101137230 <1101137230@cc.kuas.edu.tw>
2015-04-10 16:31:14 +0800

    1

---
+++ KuasCore/Dao/Impl/NewEmployeeDao.cs
@@ -0,0 +1,34 @@
+﻿
+using KuasCore.Dao.Base;
+using KuasCore.Dao.Mapper;
+using KuasCore.Models;
+using Spring.Data.Common;
+using Spring.Data.Generic;
+using System.Collections.Generic;
+using System.Data;
+
+namespace KuasCore.Dao.Impl
+{
+    public class NewEmployeeDao :  IEmployeeDao
+    {
+        public IList<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee employees1 = new Employee();
+            employees1.Id = "Four";
+            employees1.Name = "托比亞";
+            employees1.Age = 30;
+            employees.Add(employees1);
+
+            Employee employees2 = new Employee();
+            employees2.Id = "PaulWalker";
+            employees2.Name = "保羅沃克";
+            employees2.Age = 40;
+            employees.Add(employees2);
+
+            return employees;
+        }
+        
+
+    }
+}
